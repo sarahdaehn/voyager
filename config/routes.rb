f4b44e10368a4ae00deff105a4a4ef1c45f5de15
MacBook <macbook@MacBooks-MacBook-Pro.local>
2022-04-15 21:03:42 +0200

    Sign Up works

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root "cities#index"
+  devise_for :users
   resources :cities
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "cities#index"
+  
+  
 end
